e5a3ab2f499cff7783d5fd2229642adb803b5486
Marie Otsuka <motsuka.gd@gmail.com>
2022-07-22 08:35:04 -0400

    update part parsing

---
+++ docs/assets/js/plot.js
@@ -0,0 +1,263 @@
+let data_index = {};
+//get the json index file
+fetch('/assets/data/plots_web.json')
+  .then(response => response.json())
+  .then(data => {
+    data_index = data;
+    console.log(data_index);
+    intializePlots();
+  });
+
+
+//loop through figure placeholders
+function intializePlots(){
+  const plots =  document.querySelectorAll('.plot');
+
+  plots.forEach( el =>{
+    let key = el.id;
+    
+    data_index.forEach(function(p){
+      if (p.id == key){
+        let plot_data = p;
+
+        //setup DOM elements
+        let info = `
+          <div class="plot-title">${plot_data.title} <span class="fig-no">${plot_data.plot_key}</span></div>
+          <div class="plot-content"></div>
+          <div class="plot-labels"></div>
+          `;
+        el.innerHTML = info;
+
+        // plot the data into that figure
+        setup(key+'.json', key);
+      }
+    });
+
+    let colors = el.getAttribute('data-colors').split(',');
+    let color_settings = "";
+
+    // setup Dynamic CSS Styles
+    colors.forEach( (color,i) =>{
+      color_settings += 
+          `--color${i}: var(--${color.replace(/\s/g, '')});`
+    });
+    console.log(color_settings);
+    el.style.cssText = color_settings;
+    //setup graphs
+    // let plot_data = data_index[key];
+
+    // let color_settings = '';
+
+
+  });
+
+}
+
+let margin = {top: 10, right: 70, bottom: 30, left: 60};
+let width = 700 - margin.left - margin.right;
+let height = 500 - margin.top - margin.bottom;
+
+function setup( file, key ){
+
+  // append the svg object to the body of the page
+  // this contains all plot components
+  let svg = d3.select('#'+key).select('.plot-content')
+  .append("svg")
+    .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  //get the json file
+  fetch('/assets/data/'+file)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      plotData(data, svg);
+    });
+}
+
+function plotData(data, svg) {
+  
+  const z = data.z;
+  const z2 = Math.pow(data.z, 2); //z-squared
+
+	// const n_bins = bins.length - 1;
+	// console.log(bins);
+
+  const min_age = data.x[0];
+  const max_age = data.x[1];
+  const max_y = data.yt.at(-1);
+
+  // Add X axis
+  let x = d3.scaleLinear()
+      .domain([ min_age, 65])
+      .range([0, width])
+      .clamp(true)
+
+
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  let y = d3.scaleLinear()
+    .domain([0, max_y])
+    .range([ height, 0 ])
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  let area = d3.area()
+      .x(d => x(d.x))
+      .y1(d => y(d.high))
+      .y0(d => y(d.low));
+
+  let line = d3.line()
+      .x(d => x(d.x))
+      .y(d => y(d.percent));
+
+
+  function drawError(svg, errordata, index){
+    svg.append("path")
+        .datum(errordata)
+        .attr("class", "area series"+ index)
+        .attr("d", area);
+  }
+
+  function drawLine(svg, linedata, index){
+    console.log(linedata);
+    svg.append("path")
+        .datum(linedata)
+        .attr("class", "line series"+ index)
+        .attr("d", line);
+
+    // adds series labels at the end of each line
+    let label = data.series[index].s;  
+    let container = svg.node().parentNode.parentNode.nextElementSibling;
+
+    let ypos = 75*( y(linedata[linedata.length-1].percent) / height );
+    console.log(ypos);
+    let label_el = document.createElement("div");
+    label_el.innerText = label;
+    label_el.classList.add("label", "series"+index);
+    label_el.style.cssText = `top: ${ypos}%`;
+    container.append(label_el);
+    
+    // svg.append("text") //dont use svg text because they won’t wrap
+    //   .attr("transform", "translate(" + (width+3) + "," +  + ")")
+    //   .attr("dy", ".35em")
+    //   .attr("text-anchor", "start")
+    //   .attr("class", "label plot"+ index)
+    //   .text(label);
+  }
+
+
+  function drawBins(svg, data){    
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bin")
+        .attr("x", d => x(d.x) )
+        .attr("y", 0)
+        .attr("width", d => {
+          return (x(d.x + d.width) - x(d.x) )
+        })
+        .attr("height", height);
+  }
+
+  
+  const ages = Array
+    .from({length: max_age + 1 - min_age}, (v, i) => i)
+    .map(i => i + min_age); // array of integers beetween
+
+  const bins = data.bins;
+  const series = data.series;
+
+  let dataset = [];
+  let errorset = [];
+  let rects = [];
+
+  series.forEach( (values, i) =>{
+      //for each series
+      dataset[i] = []; //data for plotting
+      errorset[i] = []; //data for drawing bins
+      rects[i] = []; //data for drawing bins
+
+      //calculate error bars for each bin
+      for(let j=0; j < bins.length - 1; j++){
+
+        let bin_bounds = [];
+
+        if ( j == bins.length - 1 ){
+          bin_bounds = [bins[j], max_age ]; //last bin?
+        }else{
+          bin_bounds = [bins[j], bins[j+1] ];
+        }
+
+       
+        let ages_bounds = [ ages.indexOf(bin_bounds[0]), ages.indexOf(bin_bounds[1]) ];
+        let ages_mask = ages.slice( ages_bounds[0], ages_bounds[1]);
+
+        let diff = ages_bounds[1] - ages_bounds[0];
+        let ages_avg = d3.mean(ages_mask);
+        // console.log(ages_avg, diff);
+       
+        // console.log('\nbounds', bin_bounds, ages_bounds, ages_mask);
+
+        let subset_n = values['n'].slice(ages_bounds[0], ages_bounds[1]);
+        let subset_p = values['p'].slice(ages_bounds[0], ages_bounds[1]);
+        let subset_N = values['N'].slice(ages_bounds[0], ages_bounds[1]);
+        let subset_P = values['P'].slice(ages_bounds[0], ages_bounds[1]);
+
+        let sum_n = d3.sum(subset_n);
+        let sum_p = d3.sum(subset_p);
+        let sum_N = d3.sum(subset_N);
+        let sum_P = d3.sum(subset_P); 
+
+        // console.log(sum_n, sum_p, sum_N, sum_P);
+
+        let n = sum_n + sum_p;
+        let nz2 = n + z2;
+        let w = z2 / nz2;
+        let w1 = 1 - w;
+
+        let p = sum_P / (sum_N + sum_P); // weighted yes fraction
+        let p0 = w1*p + w*0.5;
+        let pe = z * Math.sqrt(w1 * p * (1 - p)/nz2 + w/(4 * nz2) );
+
+        let percent = 100 * p;
+
+        let error_area = {
+          x: ages_avg,
+          low: 100 * (p0 - pe),
+          high: 100 * (p0 + pe),
+        }
+
+        let datapoint = {
+          x: ages_avg,
+          percent: percent
+        }
+
+        let bin = {
+          x: bin_bounds[0],
+          width: diff
+        }
+
+        dataset[i].push(datapoint);
+        errorset[i].push(error_area);
+
+        if(i == 0){
+          //this only needs to happen for the first series
+          rects[i].push(bin); 
+        }        
+      };
+
+      drawLine(svg, dataset[i], i); //svg container, data, index for CSS styling
+      drawError(svg, errorset[i], i);
+  });
+
+  // console.log(rects);
+  drawBins(svg, rects[0]);
+
+}
---
+++ docs/assets/js/script.js
@@ -0,0 +1,285 @@
+
+$(function(){
+
+//SMOOTH SCROLLING
+		$(document).on('click', 'a[href^="#"]', function (event) {
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
+			}, 500);
+		});
+// Hide Header on on scroll down
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 5;
+		var navbarHeight = $('.navlink').outerHeight();
+
+		$(window).scroll(function(event){
+			didScroll = true;
+		});
+
+		setInterval(function() {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, 250);
+
+		function hasScrolled() {
+			var st = $(this).scrollTop();
+			
+			// Make sure they scroll more than delta
+			if(Math.abs(lastScrollTop - st) <= delta)
+				return;
+			
+			// If they scrolled down and are past the navbar, add class .nav-up.
+			// This is necessary so you never see what is "behind" the navbar.
+			if (st > lastScrollTop && st > navbarHeight){
+				// Scroll Down
+				$('.navlink').removeClass('nav-down').addClass('nav-up');
+			} else {
+				// Scroll Up
+				if(st + $(window).height() < $(document).height()) {
+					$('.navlink').removeClass('nav-up').addClass('nav-down');
+				}
+			}
+			
+			lastScrollTop = st;
+		}
+// FOOTNOTE POPUPS
+		const popup = $('#footnote-popup');
+
+		$('.reference').on('click', function(e){
+			
+		 		let id = $(this).attr('id').replace('fnref', '');
+				// let id_x = this.getBoundingClientRect().top;
+				// let id_y = this.getBoundingClientRect().left;
+				var offset = $(this).offset();
+				var id_x = offset.top;
+				var id_y = offset.left;
+			
+		 		let copy = $('#fn'+id);
+		 		$('.footnote-backref', copy).remove();
+
+		 		let popup_text = copy.html();
+		 		popup.html(popup_text);
+
+		 		// console.log('#fnitem'+id);
+		 		if (popup.hasClass('visible')){
+		 			popup.removeClass('visible');
+		 		} else{
+		 			// $('.visible').removeClass('visible');
+		 			popup.addClass('visible');
+					popup.css({
+						'top': id_x + 'px',
+						'left': id_y + 20 + 'px'
+					});
+		 		}
+		});
+		
+		$('*').click(function(e) {
+			if ($(e.target).hasClass('footnote-ref')) {
+		
+			} else {
+				popup.removeClass('visible');
+			}
+		});
+		
+
+// IMAGE TREATMENT
+	// let img = $('.periphery');
+	// $( img ).each(function() {
+	//   let width = Math.random() * window.innerWidth / 2.0;
+	//   let height = Math.random() * window.innerHeight / 2.0;
+	//   let x = Math.random() * (window.innerWidth - width);
+	//   let y = Math.random() * (window.innerHeight - height);
+	//   console.log(x);
+
+	//   $(this).css({
+	//     left: x + 'px',
+	//     top: y  + 'px',
+	//     width: width  + 'px'
+	//   });
+	// });
+
+	// const scroller = scrollama();
+
+	// // setup the instance, pass callback functions
+	// scroller
+	//   .setup({
+	//     step: ".periphery",
+	//   })
+	//   .onStepEnter((response) => {
+	//     // { element, index, direction }
+	//     response.element.classList.add('img-on');
+	//     console.log('intersecting');
+	//   })
+	//   .onStepExit((response) => {
+	//   	response.element.classList.remove('img-on');
+	//     // { element, index, direction }
+	//   });
+
+// GLOBAL: navigation toggle
+	  $('.current-page').click(function() {
+		$('ul.toc').toggleClass('display');
+	  });
+// HOME: image treatment
+
+	//   let img = $('.bg img');
+	//   $( img ).each(function() {
+	// 	let width = Math.random() * window.innerWidth / 2.0;
+	// 	let height = Math.random() * window.innerHeight / 2.0;
+	// 	let x = Math.random() * (window.innerWidth - width);
+	// 	let y = Math.random() * (window.innerHeight - height);
+	// 	console.log(x);
+  
+	// 	$(this).css({
+	// 	  left: x + 'px',
+	// 	  top: y  + 'px',
+	// 	  width: width  + 'px'
+	// 	});
+	//   });
+
+    /*------------------------------------------------
+     Scroll-based elements
+     ------------------------------------------------*/
+
+		// //------------responsive column with a plot
+		// const card = document.querySelectorAll('.col');
+		// const observer = new IntersectionObserver(
+		// 	entries =>{
+		// 		entries.forEach(entry => {
+		// 		// entry.target.classList.toggle('is-active', entry.isIntersecting);
+
+		// 		entry.target.classList.toggle('on', entry.isIntersecting);
+		// 		// entry.target.previousElementSibling.classList.toggle('on-prev', entry.isIntersecting);
+		// 		// entry.target.nextElementSibling.classList.toggle('on-next', entry.isIntersecting);
+
+		// 		});
+		// 		console.log(entries);
+		// 	}, {
+		// 		threshold: 0.2, //percentage of the element exposure in the viewport
+		// 		// root: '.col-r', //root container
+		// 		rootMargin: "100px"
+		// 	}
+
+		// );
+
+
+		// card.forEach(card => {
+		// observer.observe(card);
+		// })
+
+		// //------------toggle secondary images
+		// const col = document.querySelectorAll('.cols');
+		// const observer2 = new IntersectionObserver(
+		// entries =>{
+		// 	entries.forEach(entry => {
+		// 	entry.target.classList.toggle('on-img', entry.isIntersecting);
+		// 	if(entry.isIntersecting) {
+		// 		//position seconrdary images
+		// 		// let img = $('.on-img .peripheral');
+		// 		// $( img ).each(function() {
+		// 		//   let width = Math.random() * window.innerWidth / 2.0;
+		// 		//   let height = Math.random() * window.innerHeight / 2.0;
+		// 		//   let x = Math.random() * (window.innerWidth - width);
+		// 		//   let y = Math.random() * (window.innerHeight - height);
+		// 		//   console.log(x);
+			
+		// 		//   $(this).css({
+		// 		//     left: x + 'px',
+		// 		//     top: y  + 'px'
+		// 		//     // width: width  + 'px'
+		// 		//   });
+		// 		// });
+		// 	}
+			
+		// 	});
+		// 	console.log(entries);
+		// }, {
+		// 	threshold: 0.1 
+		// }
+
+		// );
+
+		// col.forEach(col => {
+		// observer2.observe(col);
+		// })
+
+		// //------------interlude
+		// const cover = document.querySelectorAll('.interlude');
+		// const observer3 = new IntersectionObserver(
+		// entries =>{
+		// 	entries.forEach(entry => {
+		// 	entry.target.parentElement.classList.toggle('black', entry.isIntersecting);
+			
+		// 	});
+		// 	console.log(entries);
+		// }, {
+		// 	threshold: 0.5, 
+		// 	rootMargin: "20px"
+		// }
+
+		// );
+
+		// cover.forEach(cover => {
+		// observer3.observe(cover);
+		// })
+
+
+});
+
+
+
+// /*------------------------------------------------
+//   split each line of the text and wrap it in span https://stackoverflow.com/questions/60963965/how-to-split-and-wrap-lines-in-a-span-in-each-section
+//   ------------------------------------------------*/
+//  function wrapNewLines(targetSelector, wrapEl = 'span', wrapClass = 'line') {
+//   const content = document.querySelectorAll(targetSelector)
+//   content.forEach(section => {
+//     let sectionWidth = section.getBoundingClientRect().width
+//     // let sectionWidth =  window.innerWidth/2;
+//     // console.log('column:' + sectionWidth);
+//     let words = section.innerText.split(/( )/g);
+//     section.innerHTML = words.map(word =>`<span>${word}</span>`).join('')
+//     let lines = []
+//     let line = []
+//     let lineWidth = 0
+//     let spans = section.querySelectorAll('span')
+//     spans.forEach((span, i) => {
+//       let spanWidth = span.getBoundingClientRect().width;
+//       // console.log('span:'+ spanWidth);
+//       // if (lineWidth + spanWidth <= sectionWidth - 4) {
+//         if (lineWidth + spanWidth <= sectionWidth - (sectionWidth/1.8)) {
+
+//         line.push(span)
+//         lineWidth += spanWidth
+//       } else {
+//         lines.push(line)
+//         line = []
+//         lineWidth = 0
+//         line.push(span)
+//         lineWidth += spanWidth
+//       }
+     
+//     })
+//     if (line.length) lines.push(line)
+//     let newLines = lines
+//       .map(
+//         line =>
+//           `<${wrapEl} class=${wrapClass}>${line
+//             .map(span => span.innerText)
+//             .join('')}</${wrapEl}>`
+//       )
+//       .join('')
+//     section.innerHTML = newLines
+//   })
+// }
+
+//  $(window).on('resize', function(){
+//   wrapNewLines ('.col p');
+// });
+// $(document).ready(function(){
+// 	wrapNewLines ('.col p');
+// });
